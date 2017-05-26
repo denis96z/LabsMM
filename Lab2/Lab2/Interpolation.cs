7f257cf7203687eefe26260d562af90ab4cdcf6d
Dima Orlov <nphoxconnect@gmail.com>
2017-05-26 05:11:47 +0300

    Implemented interpolation

--- Lab2/Lab2/Interpolation.cs
+++ Lab2/Lab2/Interpolation.cs
@@ -58,12 +58,16 @@ abstract class Interpolation2
         this.xValues = x;
         this.yValues = y;
         this.zValues = z;
+        X_NODES_COUNT = x.Length;
+        Y_NODES_COUNT = y.Length;
     }
 
     public abstract double FindValue(double x, double y);
 
     protected double[] xValues = null, yValues = null;
     protected double[,] zValues = null;
+    protected readonly int X_NODES_COUNT = 0;
+    protected readonly int Y_NODES_COUNT = 0;
 }
 
 /// <summary>
@@ -76,7 +80,37 @@ class LinearInterpolation2 : Interpolation2
 
     public override double FindValue(double x, double y)
     {
-        throw new NotImplementedException();
+        int index1_X = FirstIndex(x, xValues, X_NODES_COUNT);
+        int index2_X = index1_X + 1;
+
+        int index1_Y = FirstIndex(y, yValues, Y_NODES_COUNT);
+        int index2_Y = index1_Y + 1;
+
+
+        double r1 = (xValues[index2_X] - x) / (xValues[index2_X] - xValues[index1_X]) * zValues[index1_X, index1_Y] +
+        	(x - xValues[index1_X]) / (xValues[index2_X] - xValues[index1_X]) * zValues[index2_X, index1_Y];
+
+
+		double r2 = (xValues[index2_X] - x) / (xValues[index2_X] - xValues[index1_X]) * zValues[index1_X, index2_Y] +
+        	(x - xValues[index1_X]) / (xValues[index2_X] - xValues[index1_X]) * zValues[index2_X, index2_Y];
+        	
+
+        double result = (yValues[index2_Y] - y) / (yValues[index2_Y] - yValues[index1_Y]) * r1 +
+        	(y - yValues[index1_Y]) / (yValues[index2_Y] - yValues[index1_Y]) * r2;
+
+        return result;
+    }
+
+    private int FirstIndex(double value, double[] conteiner, int nodesCount)
+    {
+        for (int i = 1; i < nodesCount - 1; i++)
+        {
+            if (value <= conteiner[i])
+            {
+                return i - 1;
+            }
+        }
+        return nodesCount - 2;
     }
 }
 
@@ -88,8 +122,39 @@ class LinearLogInterpolation2 : Interpolation2
     public LinearLogInterpolation2(double[] x,
         double[] y, double[,] z) : base(x, y, z) { }
 
+
     public override double FindValue(double x, double y)
     {
-        throw new NotImplementedException();
+        int index1_X = FirstIndex(x, xValues, X_NODES_COUNT);
+        int index2_X = index1_X + 1;
+
+        int index1_Y = FirstIndex(y, yValues, Y_NODES_COUNT);
+        int index2_Y = index1_Y + 1;
+
+
+        double r1 = (xValues[index2_X] - x) / (xValues[index2_X] - xValues[index1_X]) * Math.Log(zValues[index1_X, index1_Y]) +
+        	(x - xValues[index1_X]) / (xValues[index2_X] - xValues[index1_X]) * Math.Log(zValues[index2_X, index1_Y]);
+
+
+		double r2 = (xValues[index2_X] - x) / (xValues[index2_X] - xValues[index1_X]) * Math.Log(zValues[index1_X, index2_Y]) +
+        	(x - xValues[index1_X]) / (xValues[index2_X] - xValues[index1_X]) * Math.Log(zValues[index2_X, index2_Y]);
+        	
+
+        double result = (yValues[index2_Y] - y) / (yValues[index2_Y] - yValues[index1_Y]) * Math.Exp(r1) +
+        	(y - yValues[index1_Y]) / (yValues[index2_Y] - yValues[index1_Y]) * Math.Exp(r2);
+
+        return result;
+    }
+
+    private int FirstIndex(double value, double[] conteiner, int nodesCount)
+    {
+        for (int i = 1; i < nodesCount - 1; i++)
+        {
+            if (value <= conteiner[i])
+            {
+                return i - 1;
+            }
+        }
+        return nodesCount - 2;
     }
 }
